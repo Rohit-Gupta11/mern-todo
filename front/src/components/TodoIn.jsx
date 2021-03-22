734bce7b5e4c1d24ce1c12eb7e430ea596f8a44c
Rohit Gupta <grohit7711@gmail.com>
2021-03-22 20:38:26 +0530

    Revert "some states created in todoIn"
    This reverts commit 698b38e3936fdfa1b164ae184e308df1184bb0b7.

--- front/src/components/TodoIn.jsx
+++ front/src/components/TodoIn.jsx
@@ -1,38 +1,21 @@
 import React from "react";
 import "./TodoIn.css";
-import { useState } from "react";
 
 const TodoIn = () => {
-    const [todoDescription, setDescription] = useState('');
-    const [todoTitle, setTitle] = useState('');
-
-    function onSubmitTodo() {
-        let dataGain = {
-            title: todoTitle,
-            description: todoDescription,
-            date: Date.now()
-        }
-        try {
-            console.log(dataGain);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     return (
         <div className="container mg-bt">
             <h1>Ok You got new task. Enter ASAP !!</h1>
             <form action="POST">
                 <div className="">
                     <label>Todo Title </label>
-                    <input onChange={e => setTitle(e.target.value)} type="text" placeholder="enter title " />
+                    <input className="" type="text" placeholder="enter title " />
                 </div>
                 <div className="">
                     <label>Todo Description </label>
-                    <textarea onChange={e => setDescription(e.target.value)} cols="30" rows="10" placeholder="enter description "></textarea>
+                    <textarea className="" name="" id="" cols="30" rows="10" placeholder="enter description "></textarea>
                 </div>
             </form>
-            <a href="/todo" onClick={onSubmitTodo()}>Submit</a>
+            <a href="/todo">Submit</a>
             <a href="/">Cancel</a>
         </div>
     );
